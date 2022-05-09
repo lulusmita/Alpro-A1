1472a30e1a34835e666a910f7a755d5154b80f48
lulusmita <88651974+lulusmita@users.noreply.github.com>
2022-05-09 07:25:54 +0700

    Update InsertionSort.c

--- Praktikum 9/InsertionSort.c
+++ Praktikum 9/InsertionSort.c
@@ -1,31 +1,39 @@
 /* Nama file     :InsertionSort.c */
-/* Deskripsi     :Menyortir suatu bilangan dengan metode insertion sort */
+/* Deskripsi     :Menyortir suatu bilangan dari urutan terbesar ke terkecil atau descending dengan metode insertion sort */
 /* Pembuat       :Lulus Dwiyan Mita-24060121120029 */
 /* Tgl Pembuatan :Senin,2 Mei 2022/14.55 WIB */
 #include<stdio.h>
 #include<stdlib.h>
+/*Fungsi insertionSort*/
 void insertionSort(int array[],int n){
-    int i,j,temp;
+    int i,j,ts;
     for(i=1;i<n;i++){
-        temp=array[i];
+        ts=array[i];
         j=i-1;
-        while(j>=0&&array[j]>temp){
+        while(j>=0&&array[j]>ts){
             array[j+1]=array[j];
             j=j-1;
         }
-        array[j+1]=temp;
+        array[j+1]=ts;
     }
 }
 int main(){ /* Program Utama */
 /* Kamus */
-   int i,n,array[]={29,10,14,37,14,13,5,7,40,35};
+   int i,N,*array;
 /* Algoritma */
    printf("=========================================================================\n");
    printf("                        Insertion Sort Program                           \n");
    printf("=========================================================================\n");
-   n=sizeof(array)/sizeof(array[0]);
-   insertionSort(array,n);
-   for(i=n-1;i>=0;i--){
+   printf("Masukkan panjang array:");scanf("%d", &N);
+   array = (int*) malloc(N * sizeof(int));
+   printf("Masukkan elemen-elemen array:\n");
+    for(i=0;i<N;i++){
+        scanf("%d", &array[i]);
+    }
+   printf("=========================================================================\n");
+   insertionSort(array,N);
+   printf("Array setelah disorting secara descending dengan insertion sort:\n");
+   for(i=N-1;i>=0;i--){
     printf("%d ",array[i]);
    }
    printf("\n=========================================================================\n");
